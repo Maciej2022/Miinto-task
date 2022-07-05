d70bfd1c033af382dbbb539662f7918f661e0c71
Maciej2022 <maciekzablocki1@gmail.com>
2022-07-05 20:37:07 +0200

    python class

---
+++ balon.py
@@ -0,0 +1,12 @@
+from typing_extensions import Self
+
+
+class Balon:
+    def __init__(self, string_nazwa, float_wysokosc, float_srednica):
+    self.string_nazwa = string_nazwa
+    self.float_wysokosc = float_wysokosc
+    self.float_srednica = float_srednica
+
+
+def zmien_wysokosc(wysokosc):
+    print(wysokosc + self.float_wysokosc)
